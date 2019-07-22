b5f5d7ce701bb9276060accf23340dae4fe301e1
Valentin Mihov <valentin.mihov@gmail.com>
2019-07-22 12:11:48 +0300

    Use `Santiment Data` instead of `Grafana` for the page title

--- public/app/features/dashboard/containers/DashboardPage.test.tsx
+++ public/app/features/dashboard/containers/DashboardPage.test.tsx
@@ -116,7 +116,7 @@ describe('DashboardPage', () => {
     });
 
     it('Should update title', () => {
-      expect(document.title).toBe('My dashboard - Grafana');
+      expect(document.title).toBe('My dashboard - Santiment Data');
     });
 
     it('Should render dashboard grid', () => {
